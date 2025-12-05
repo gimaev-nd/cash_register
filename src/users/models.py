caf685df7c6db5865ce6326c7475f06772d73abc
Nail Gimaev <gimntut@list.ru>
2025-12-05 15:25:30 +0500

    fuff fix

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-
-
+#
 class Gamer(models.Model):
-    name = models.CharField("Игрок", max_length=50)
+    name: str = models.CharField("Игрок", max_length=50)  # pyright: ignore[reportUnknownVariableType, reportAssignmentType]
