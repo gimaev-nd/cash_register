d7d281b6d86a08c9ada847b065b1107f9b3c488b
Nail Gimaev <gimntut@list.ru>
2025-12-05 13:59:20 +0500

    Migration commands in Makefile

--- src/config/settings.py
+++
@@ -1,1 +0,0 @@
-from .default_django import *  # noqa: F403
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Gamer(models.Model):
+    name = models.CharField("Игрок", max_length=50)
