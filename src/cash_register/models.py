4c4bc595c0d10e770a8883e29ab860533a0cd312
Nail Gimaev <gimntut@list.ru>
2025-12-05 15:50:13 +0500

    Game model

--- src/cash_register/models.py
+++ src/cash_register/models.py
@@ -1,2 +1,29 @@
+from pyexpat import model
+from tkinter import N
+from turtle import mode
+from typing import Any, TypedDict, cast
 
-# Create your models here.
+from django.db import models
+
+
+class GameDataV1(TypedDict):
+    pass
+
+
+def parse(game):  # pyright: ignore[reportMissingParameterType, reportUnknownParameterType]
+    return cast(GameDataV1, game)
+
+
+class Game(models.Model):
+    gamer = models.ForeignKey(
+        "users.Gamer", models.CASCADE, "games", verbose_name="Игрок"
+    )
+    game = models.JSONField("Данные игры")
+    version = models.IntegerField("Версия схемы сотояния игры")
+
+    def get_game_data(self) -> GameDataV1:
+        self.update_version()
+        return parse(self.game)  # pyright: ignore[reportAny]
+
+    def update_version(self) -> None:
+        pass
--- src/config/settings/n20_project.py
+++ src/config/settings/n20_project.py
@@ -1,4 +1,7 @@
 from .n10_default_django import INSTALLED_APPS as DJANGO_APPS
 
-PROJECT_APPS = ['users.apps.UsersConfig']
+PROJECT_APPS = [
+    "users.apps.UsersConfig",
+    "cash_register.apps.CashRegisterConfig",
+]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
