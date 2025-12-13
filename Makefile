aa8c2c1fbceff42eb3b85d5bb939b6b34aa85956
Nail Gimaev <gimntut@list.ru>
2025-12-13 14:19:53 +0500

    Test get_game_by_gamer_name

--- Makefile
+++ Makefile
@@ -2,20 +2,28 @@
 m1:
 	src/manage.py makemigrations
 	uv run ruff check --fix
+
 .PHONY: m2
 m2:
 	src/manage.py migrate
+
 .PHONY: mm
 mm:
 	src/manage.py makemigrations
 	src/manage.py migrate
 	uv run ruff check --fix
+
 .PHONY: del_db
 del_db:
 	rm -f src/db.sqlite3||echo File not found
+
 .PHONY: cmm
 cmm: del_db mm
 
 .PHONY: check
 check:
 	uv run ruff check --fix
+
+.PHONY: test
+test:
+	pytest --lf --cov --cov-report html
--- tests/gamer/test_game.py
+++ tests/gamer/test_game.py
@@ -1,6 +1,7 @@
 import pytest
 
-from cash_register.services.game import get_game
+from cash_register.models import Game
+from cash_register.services.game import get_game, get_game_by_gamer_name
 from users.models import Gamer
 from users.services.gamer import get_gamer
 
@@ -12,8 +13,33 @@ def gamer() -> Gamer:
     return get_gamer("Вася")
 
 
+@pytest.fixture
+def game(gamer) -> Game:
+    return get_game(gamer)
+
+
 def test_create_game(gamer):
     game = get_game(gamer)
 
     assert get_game(gamer) is not None
     assert game.gamer.name == "Вася"
+
+
+def test_success_get_game_by_gamer_name(game):
+    given_game = get_game_by_gamer_name("Вася")
+
+    assert given_game.gamer.name == "Вася"
+    assert given_game == game
+
+
+def test_success_create_get_game_by_gamer_name():
+    game = get_game_by_gamer_name("Вася")
+
+    assert game.gamer.name == "Вася"
+
+
+@pytest.mark.usefixtures("game")
+def test_fail_create_get_game_by_gamer_name():
+    game = get_game_by_gamer_name("Петя")
+
+    assert game.gamer.name != "Вася"
